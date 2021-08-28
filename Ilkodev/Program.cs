ccce476bd3801178f0d63cc77638622443c52f86
KeremAyseli <keremayseli@outlook.com>
2021-08-28 06:34:23 +0300

    IlkOdevFirstCommit

---
+++ Ilkodev/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ilkodev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz");
+            Console.WriteLine("Merhaba {0}",Console.ReadLine().ToString());
+        }
+    }
+}
