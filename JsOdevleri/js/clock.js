0ba44c1f684d9f168625a0fb25609ec7191bd001
keremayseli <keremayseli@outlook.com>
2021-11-11 01:46:44 +0300

    jsOdev

--- JsOdevleri/js/clock.js
+++ JsOdevleri/js/clock.js
@@ -3,3 +3,8 @@ function showTime(){
     document.getElementById("myClock").innerText=(d.getHours().toString()+":"+d.getMinutes().toString()+":"+d.getSeconds().toString());
      setTimeout(showTime,1000);
 }
+showTime();
+
+function Onload(){
+    document.getElementById("myName").innerText=prompt("İsminiz nedir?").toUpperCase();
+}
